3bab7f93495f891f5deee6a936cb8d3f39d4b470
nikayel <alinikayeljamal@gmail.com>
2023-08-07 14:31:13 -0700

    make institial models

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,40 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class event(models.Model):
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     date = models.DateField()
+    location = models.CharField(max_length=255)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    catagory = models.ForeignKey('catagory', on_delete=models.CASCADE)
+    vendor = models.ManyToManyField('vendor')
+    participants = models.integerField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name}, {self.id}'
+
+
+class catagory(models.Model):
+    type = models.CharField(max_length=255)
+    description = models.TextField()
+    age_group = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name}, {self.id}'
+
+class vendor(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    phone_number = models.CharField(max_length=255)
+    email = models.EmailField()
+    pricing = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name}, {self.id}'
+
+class rating(models.Model):
+    value = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.value}, star'
