e904d6343bab2701863751e6d1c22a7a6ffe6cbd
nikayel <alinikayeljamal@gmail.com>
2023-08-08 10:29:20 -0700

    Fix the become vendor

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+A
+main_app/tests.py,5/5/55ca8abdd42211561f9c2bae4292978925ca8132
--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Event, Vendor, Rating
 # Register your models here.
+admin.site.register(Vendor)
+admin.site.register(Event)
