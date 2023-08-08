2a5461902d1d13bd1fe2a89ab3dda5bccfeedf7c
ddesrochers99 <desrochersdale@gmail.com>
2023-08-08 16:06:33 -0700

    makes random changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('become_vendor/', views.become_vendor, name='become_vendor'),
     path('event/create/', views.EventCreate.as_view(), name="create_event"),
     path('upcoming_events/', views.upcoming_events, name='upcoming_events'),
-    path('events/<int:event_id>/', views.event_detail, name='event_detail'),
+    # path('events/<int:event_id>/', views.event_detail, name='event_detail'),
 ]
