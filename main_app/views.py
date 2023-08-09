52c3d0e6d5ebe7ec41c4cf04266757d9b484beed
ddesrochers99 <desrochersdale@gmail.com>
2023-08-08 17:12:19 -0700

    Fixes signup redirect

--- main_app/views.py
+++ main_app/views.py
@@ -90,7 +90,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('index')
+      return redirect('upcoming_events')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
